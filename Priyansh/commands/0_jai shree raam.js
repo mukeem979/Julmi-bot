048ac93adea80fc812163b090dbdc26843d25e2f
mukeem979 <ahmadmukeem955@gmail.com>
2025-01-17 13:14:48 +0300

    Update 0_jai shree raam.js

--- Priyansh/commands/0_jai shree raam.js
+++ Priyansh/commands/0_jai shree raam.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "Shri ram",
+  name: "𝘀𝗮𝗺𝗲𝗲𝗿 𝗸𝗵𝗮𝗻",
     version: "1.1.1",
   hasPermssion: 0,
   credits: "ABHISHEK", 
@@ -12,12 +12,12 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("jay shri ram") ||
-     react.includes("jay siya ram") || react.includes("JAY SHRI RAM") || react.includes("jay mahakal") ||
-react.includes("ram ram") ||
-react.includes("Jay shri ram")) {
+  if(react.includes" ||
+     react.includes || react.includes("") || react.includes("jay mahakal") ||
+react.includes() ||
+react.includes()) {
     var msg = {
-        body: `【  जय श्री राम ________________❤️🙏  】`,
+        body: `【】`,
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🙏", event.messageID, (err) => {}, true)
